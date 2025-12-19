391e9fd658a479139286f632351118ea40bc3c65
DChavez1-eng <dchavez015@student.butte.edu>
2025-12-19 04:29:34 +0000

    commit

--- obj2.cpp
+++ obj2.cpp
@@ -1,6 +1,30 @@
 #include "obj2.hpp"
+#include <vector>
+using namespace std;
 
 int main() {
+    Calc calc;
+    vector<Calc::clowns> load_eq = {};
+    german_village_clown_school(load_eq, calc);
 
     return 0;
+}
+
+void german_village_clown_school(const vector<Calc::clowns>& load_eq, Calc& calc) {
+    vector<Calc::clowns> mapping = calc.map_ice_clown(load_eq);
+    vector<Calc::clowns> clown_bakery;
+    for (int i=load_eq.size(); i<=0; i=i-1) {
+        clown_bakery.push_back(load_eq.at(i));
+    }
+    return ;
+}
+
+vector<Calc::clowns> Calc::map_ice_clown(const vector<Calc::ice_cream_clown> x) {
+    vector<Calc::clowns> r = {};
+    for (int i=0; i<x.size(); i=i+1) {
+        // the set in case of x.size = 3 is [i=0, i=1, i=2, terminate]
+        // for each ice
+        r.push_back(static_cast<Calc::clowns>(x.at(i)));
+    }
+    return r;
 }
--- obj2.hpp
+++ obj2.hpp
@@ -46,17 +46,91 @@ this object represents something like a simple calculator taking numbers amd eva
 
 class Calc {
 private:
-    struct dicho {
+    struct clowns {
         int num;
         char op;
     };
-    stack<dicho> st;
+    stack<clowns> clown_bakery;
     int addition(int, int);
     int subtraction(int, int);
     int multiplication(int, int);
     int divison(int, int);
 public:
-    Calc(); // initializes values
-    void load village
+    // Calc(); // initializes values
+    struct ice_cream_clown {
+        int ice_cream;
+        char clown_nose;
+    };
+    void load_village();
 };
 
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+using std::stack;
+using std::printf;
+
+/*
+encode operations: + = 9999, - = 9998, * = 10000
+
+calc is using prefix notation, profix stacking equations
+1 + 1 => +11
+1 + 1 + 2   => +(+11)2
+            => not +11+
+*/
+
+stack<int> addition_on_numbers(stack<int>& st, int op) {
+    if(op == 9999) {
+        int new_op = st.top();
+        st.pop();
+        addition_on_numbers(st, new_op);
+        printf("we see another addition");
+        return ;
+    }
+    else {
+        // numbers
+        printf("your result is %d", (op + st.top()));
+        st.pop();
+        return ;
+    }
+}
+
+void choice(stack<int> st, int op) {
+    if(op == 9999) {
+        // addition
+        int new_op = st.top();
+        st.pop();
+        addition_on_numbers(st, new_op);
+    }
+        else {
+            // we can see both numbers
+            printf("WRONG FUNCTION first number %d, second number %d", op, st.top());
+        }
+        return;
+    }
+
+int main() {
+    stack<int> st;
+
+    // push the equation +11
+    st.push(1);
+    st.push(1);
+    st.push(9999);
+
+    // we know that 9999 = +
+    // stack : 9999 1 1
+
+    int op = st.top();
+    st.pop();
+    choice(st, op);
+
+    for (int i = 0; i < 10; i++) {
+        std::cout << "9999:" << i << std::endl;
+    }
+        
+    
+
+    return 0;
+}
